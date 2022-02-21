92c3ec84d0f66f792cfd854e12e08f32acc77376
gatovchenko <s1mbenok@mailru>
2022-02-21 23:20:06 +0300

    reverse_num

--- Module14/05_least_divisor/main.py
+++ Module14/05_least_divisor/main.py
@@ -1,1 +1,13 @@
 # здесь писать код
+def nok(x):
+    least = 0
+    for i in range((x + 1), 1, -1):
+        if x % i == 0:
+            least = i
+    return least
+
+while True:
+    number = int(input('Input your number: '))
+    least_nok = nok(number)
+    print(f'Least nok of {number} = {least_nok}.')
+    print()
