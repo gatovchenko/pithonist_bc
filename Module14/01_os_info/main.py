136353234157fbb22425256d2b4a7c968e7c6aa5
gatovchenko <s1mbenok@mailru>
2022-02-21 22:33:16 +0300

    PC

--- Module14/01_os_info/main.py
+++ Module14/01_os_info/main.py
@@ -1,14 +1,6 @@
 
 
-import platform
-import sys
+#OS info is
+#uname_result(system='Windows', node='DESKTOP-7024Q46', release='10', version='10.0.19043', machine='AMD64')
 
-info = 'OS info is \n{}\n\nPython version is {} {}'.format(
-    platform.uname(),
-    sys.version,
-    platform.architecture(),
-)
-print(info)
-
-with open('os_info.txt', 'w', encoding='utf8') as file:
-    file.write(info)
+#Python version is 3.9.7 (tags/v3.9.7:1016ef3, Aug 30 2021, 20:19:38) [MSC v.1929 64 bit (AMD64)] ('64bit', 'WindowsPE')
