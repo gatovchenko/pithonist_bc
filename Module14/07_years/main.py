7876f0dfe3a17b7782f46ab5c8c8477cc095d550
gatovchenko <s1mbenok@mailru>
2022-02-21 23:51:36 +0300

    years * 3

--- Module14/07_years/main.py
+++ Module14/07_years/main.py
@@ -1,1 +1,11 @@
 # здесь писать код
+start_year = int(input('Input starting year: '))
+finish_year = int(input('Input finish year: '))
+
+for i in range(start_year, finish_year + 1):
+    i1 = i % 10
+    i2 = (i // 100) % 10
+    i3 = (i // 10) % 10
+    i4 = i // 1000
+    if i1 == i2 == i3 or i1 == i3 == i4 or i2 == i3 == i4:
+        print(i)
