5709c3f7625401802068edb6dfca4bd838eab0bb
gatovchenko <s1mbenok@mailru>
2022-02-21 22:28:27 +0300

    sum and difference

--- Module14/03_sum_and_dif/main.py
+++ Module14/03_sum_and_dif/main.py
@@ -1,1 +1,22 @@
 # здесь писать код
+def summe(x):
+    summ = 0
+    while x > 0:
+        summ += x % 10
+        x //= 10
+    return summ
+
+def cifre(x):
+    count = 0
+    while x > 0:
+        count += 1
+        x //= 10
+    return count
+
+number = int(input('Input your number: '))
+sum_numbers = summe(number)
+cifre_numbers = cifre(number)
+difference = abs(sum_numbers - cifre_numbers)
+print(f'Sum of cifres = {sum_numbers}.')
+print(f'Count of cifres = {cifre_numbers}.')
+print(f'Difference sum and count = {difference}.')
