426f07e58e5218ca6f30c879476a6a8a6e3a04dd
gatovchenko <s1mbenok@mailru>
2022-02-22 00:02:03 +0300

    search_coin

--- Module14/06_coin2/main.py
+++ Module14/06_coin2/main.py
@@ -1,1 +1,11 @@
 # здесь писать код
+import math
+
+while True:
+    x = float(input('Input coordinate x: '))
+    y = float(input('Input coordinate y: '))
+    radius = float(input('Input radius of coin: '))
+    if math.sqrt(x ** 2 + y ** 2) < radius:
+        print('Coin near!')
+    else:
+        print('Search again!')
