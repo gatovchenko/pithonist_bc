99d466b2085679daa7b77bf7fc7e84f1d081ea14
gatovchenko <s1mbenok@mail.ru>
2022-03-01 15:40:59 +0300

    tournament_names

--- Module15/02_tournament/main.py
+++ Module15/02_tournament/main.py
@@ -1,1 +1,8 @@
-##
+name_list = ['Artem', 'Boris', 'Vlad', 'Gosha', 'Dima', 'Eugenii', 'Jhenya', 'Zahar']
+first_day_list = []
+count = 0
+for i in name_list:
+    if count % 2 == 0:
+        first_day_list.append(i)
+    count += 1
+print(f'First day list: {first_day_list}.')
