3877c084e1690c9ea08776f35e6d9235db82d27c
gatovchenko <s1mbenok@mail.ru>
2022-03-01 15:45:38 +0300

    unique_letters

--- Module15/06_word_analysis/main.py
+++ Module15/06_word_analysis/main.py
@@ -1,1 +1,13 @@
+word = list(input('Input word: '))
+print(word)
+unique_letters = 0
 
+for i in word:
+    same_letters = 0
+    for j in word:
+        if i == j:
+            same_letters += 1
+    if same_letters == 1:
+        unique_letters += 1
+
+print(f'Unique letters: {unique_letters}.')
