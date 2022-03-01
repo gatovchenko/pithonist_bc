dd08fb559c84bc6634b75394fb6e0621ac2f1139
gatovchenko <s1mbenok@mail.ru>
2022-03-01 15:48:31 +0300

    pallindroms

--- Module15/09_word_analysis_2/main.py
+++ Module15/09_word_analysis_2/main.py
@@ -1,1 +1,20 @@
+word = list(input('Input word: '))
+print(word)
+count = 0
+for i in word:
+    count += 1
 
+part_word1, part_word2 = '', ''
+for i in range(count):
+    if i < count // 2:
+        part_word1 += word[i]
+    elif i >= count - i:
+        part_word2 = word[i] + part_word2
+
+print(part_word1)
+print(part_word2)
+
+if part_word1 == part_word2:
+    print('Pallindrom!')
+else:
+    print('Its not a pallindrom!')
