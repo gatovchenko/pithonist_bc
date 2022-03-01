b05dac164f8d0f51db4eddb753ffe4dba1f2aabe
gatovchenko <s1mbenok@mail.ru>
2022-03-01 15:44:23 +0300

    films

--- Module15/05_movie/main.py
+++ Module15/05_movie/main.py
@@ -1,1 +1,14 @@
-
+films = ['Крепкий орешек', 'Назад в будущее', 'Таксист', 'Леон', 'Богемская рапсодия', 'Город грехов', 'Мементо', 'Отступники', 'Деревня']
+films_list = []
+number = int(input('How many films do you want add for your list: '))
+for i in range(number):
+    film_chek = input('Input name film: ')
+    for i in films:
+        if i == film_chek :
+            films_list.append(film_chek)
+            break
+    else:
+        print(f'We have not {film_chek}.')
+print('You favorite films list: ', end = '')
+for i in films_list:
+    print(i, end = ' ')
