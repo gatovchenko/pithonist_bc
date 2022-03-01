f6bb7c502f8b9f24532dc09b477aefadb1047540
gatovchenko <s1mbenok@mail.ru>
2022-03-01 15:46:59 +0300

    scoreboard

--- Module15/08_running_nums/main.py
+++ Module15/08_running_nums/main.py
@@ -1,1 +1,12 @@
+shift = int(input('Input shift: '))
+old_scoreboard = [1, 4, -3, 0, 10]
+new_scoreboard = []
 
+count = 0
+for i in old_scoreboard:
+    count += 1
+
+for i in range(count):
+        new_scoreboard.append(old_scoreboard[i - shift])
+print(old_scoreboard)
+print(new_scoreboard)
