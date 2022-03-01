1d4b9990dc6be840f764788b27f442f418b32726
gatovchenko <s1mbenok@mail.ru>
2022-03-01 15:42:49 +0300

    ceils

--- Module15/03_cells/main.py
+++ Module15/03_cells/main.py
@@ -1,1 +1,12 @@
-
+number = int(input('Input total numbers: '))
+count = 1
+ceils_uncorrect = []
+for i in range(number):
+    efficiency = int(input(f'Input efficiency of ceil # {i+1}: '))
+    if count > efficiency:
+        ceils_uncorrect.append(efficiency)
+    count += 1
+# print(f'Uncorrect ceils: {ceils_uncorrect}')
+print('Uncorrect ceils:', end = ' ')
+for i in ceils_uncorrect:
+    print(i, end = ' ')
