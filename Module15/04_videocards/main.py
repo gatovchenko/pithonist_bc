d2916413102c26e072bb994b41a8d077d363a6e5
gatovchenko <s1mbenok@mail.ru>
2022-03-01 15:43:07 +0300

    cards

--- Module15/04_videocards/main.py
+++ Module15/04_videocards/main.py
@@ -1,1 +1,15 @@
-
+number = int(input('Input total cards: '))
+old_cardlist = []
+new_cardlist = []
+for i in range(number):
+    card = int(input(f'{i+1} Videocard: '))
+    old_cardlist.append(card)
+print(f'Old cardlist: {old_cardlist}.')
+maxnum = old_cardlist[0]
+for i in old_cardlist:
+    if i > maxnum:
+        maxnum = i
+for i in old_cardlist:
+    if i != maxnum:
+        new_cardlist.append(i)
+print(f'New cardlist: {new_cardlist}.')
