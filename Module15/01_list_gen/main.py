dbe5fb30354e6f492abe1f2ab341e35b8b3dd22e
gatovchenko <s1mbenok@mail.ru>
2022-03-01 15:39:30 +0300

    odd_numbers

--- Module15/01_list_gen/main.py
+++ Module15/01_list_gen/main.py
@@ -1,1 +1,6 @@
-#
+n = int(input('Input number: '))
+odd_numberlist = []
+for i in range(n+1):
+    if i % 2 != 0:
+        odd_numberlist.append(i)
+print(f'Odd number list to {n}: {odd_numberlist}.')
