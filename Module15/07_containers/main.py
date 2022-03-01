68acb2cd8290db849f07f90ca74f8ad4a3b13e64
gatovchenko <s1mbenok@mail.ru>
2022-03-01 15:46:21 +0300

    boxes

--- Module15/07_containers/main.py
+++ Module15/07_containers/main.py
@@ -1,1 +1,18 @@
+total_boxes = int(input('Total boxes: '))
+boxes = []
+for i in range(total_boxes):
+    weight_box = int(input(f'Weight of {i+1} box: '))
+    if weight_box > 200:
+        print(f'Weight > 200. Correction: weight {i+1} box = 200.')
+        weight_box = 200
+    boxes.append(weight_box)
 
+print(boxes)
+last_box = int(input('Weight of last box: '))
+count = 1
+for i in boxes:
+    if i >= last_box:
+        count += 1
+    else:
+        break
+print(f'Number of last box = {count}.')
