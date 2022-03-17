b21d212009829ad1f531d5b8f164cffcec9f36dc
gatovchenko <s1mbenok@mail.ru>
2022-03-17 19:34:31 +0300

    numbers 5 and 3

--- Module16/01_scary_code/main.py
+++ Module16/01_scary_code/main.py
@@ -1,1 +1,13 @@
 #
+# a = [1, 5, 3]
+# b = [1, 5, 1, 5]
+# c = [1, 3, 1, 5, 3, 3]
+#
+# a.extend(b)
+# print(f'5 = {a.count(5)}.')
+# for i in range(a.count(5)):
+#     a.remove(5)
+#
+# a.extend(c)
+# print(f'3 = {a.count(3)}.')
+# print(a)
