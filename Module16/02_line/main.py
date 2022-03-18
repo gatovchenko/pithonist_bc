635e532df7ceda8e763205446b4337e5413b63da
gatovchenko <s1mbenok@mail.ru>
2022-03-18 13:03:48 +0300

    school

--- Module16/02_line/main.py
+++ Module16/02_line/main.py
@@ -1,1 +1,12 @@
 #
+first_list = []
+second_list = []
+
+for i in range(160,177,2):
+    first_list.append(i)
+for i in range(162,181,3):
+    first_list.append(i)
+
+first_list.extend(second_list)
+first_list.sort()
+print(first_list)
